50f7a19a7543c93ec819711358042cfdbe5a26bc
Vladislav Andreev <andreev.vladislav@urfu.me>
2024-02-06 17:16:02 +0500

    Обновлено 12 задание и дабавлено условие к 10 задачи

--- section4/Task10/src/main/java/ru/andreev/Main.java
+++ section4/Task10/src/main/java/ru/andreev/Main.java
@@ -3,6 +3,11 @@ package ru.andreev;
 import java.util.Scanner;
 import java.util.stream.LongStream;
 
+/*
+Задание: Используя stream api напишите метод,
+принимающий на вход два числа a, b типа int и вычисляющий сумму чисел между числами a и b,
+включая a и исключая b. Ответ должен иметь тип long. Например: a = 3, b = 6. Ответ: 12 (3 + 4 + 5).
+*/
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
--- section4/Task11/demoapp/src/main/java/org/my/example/App.java
+++ section4/Task11/demoapp/src/main/java/org/my/example/App.java
@@ -1,9 +1,6 @@
 package org.my.example;
 
-/**
- * Hello world!
- *
- */
+//💡 Создать приложение с использованием Maven.
 public class App 
 {
     public static void main( String[] args )
